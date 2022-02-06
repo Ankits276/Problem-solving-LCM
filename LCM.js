2e9e75d8b7bbd09dbf383442fa199d009a151e51
Ankits276 <satoniyaankit@gmail.com>
2022-02-06 22:22:19 +0530

    Problem_solving LCM

---
+++ LCM.js
@@ -0,0 +1,33 @@
+/**
+ * Algo create a function for LCM
+ * Step 1:-Check input value is not a valid number or positive number
+ * Step 1.1:- If step 1 is truthy, return please enter valid number
+ * Step 2:-Itrate a input start from 2 and upto length
+ * Step 3 :-Check if input % i == 0
+ * Step 3.1:-If step 3 is truthy, push i into blank array and input divide by i and update input
+ * Step 4 :- If step 3 is falsy, then i++
+ *
+ *
+ */
+// var a = 15;
+// var b = [];
+// for (let i = 2; i <= a; ) {
+//   if (a % i == 0) {
+//     b.push(i);
+//     a = a / i;
+//   } else {
+//     i++;
+//   }
+// }
+// console.log(b);
+// Step 1:- Check if input is not a valid number or positive number
+// Step 1.1:- If step 1 is truthy, return please enter correct value,
+// Step 2:-
+
+var a = 5;
+var b;
+for (let i = 1; (i) => a; i++) {
+  b = i;
+  b = b * i;
+}
+console.log(b);
